c3157743f7f1792985b7429fbfa5aae7821ed6bd
hrenikindi <159125359+hrenikindi@users.noreply.github.com>
2024-03-23 15:01:08 +0530

    made required changes to make game functional

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Iteration 1: Making the play button in the index.html functional.
 // Description: When the play button is clicked the game.html page should be opened
-var playBtn=document.querySelector('#play-button');
+var gameStatrBtn=document.querySelector('#play-button');
 
 function game_start(){
     location.href="./game.html";
 }
 
-playBtn.addEventListener('click',game_start);
+gameStatrBtn.addEventListener('click',game_start);
